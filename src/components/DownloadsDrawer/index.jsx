579427d1eae1892834c63f3de81448a0f6c10b33
yusa-ai <64932650+yusa-ai@users.noreply.github.com>
2023-10-01 15:10:40 +0200

    Make downloads scrollable when many

--- src/components/DownloadsDrawer/index.jsx
+++ src/components/DownloadsDrawer/index.jsx
@@ -1,16 +1,19 @@
 import {
   VStack,
-  Heading
+  Heading,
+  Flex,
 } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 import DownloadProgress from './DownloadProgress';
 
 function DownloadsDrawer({ downloads }) {
   return (
-    <VStack flex={1} height="100vh" px={10} py={20} bg="teal" gap={4}>
-      <Heading as="h2" fontSize="2xl" alignSelf="flex-start" color="white" mb={4}>Downloads ({downloads.length})</Heading>
+    <VStack flex={1} height="100vh" px={10} py={20} bg="teal">
+      <Heading as="h2" fontSize="2xl" alignSelf="flex-start" color="white" mb={8}>Downloads ({downloads.length})</Heading>
 
-      {downloads.map((download) => <DownloadProgress key={download.download_uuid} download={download} />)}
+      <Flex direction="column" width="100%" gap={4} overflowY="auto">
+        {downloads.map((download) => <DownloadProgress key={download.download_uuid} download={download} />)}
+      </Flex>
     </VStack>
   );
 }
